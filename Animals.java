7449adbb0a4ae016a07798594595a5925292b446
MrStrelan <90510167+MrStrelan@users.noreply.github.com>
2021-09-11 14:39:08 +0200

    Add files via upload

---
+++ Animals.java
@@ -0,0 +1,47 @@
+public class Animals
+{
+    private String kind;
+    private int x;
+    private int y;
+
+    public Animals(String kind, int x, int y)
+    {
+        this.kind = kind;
+        this.x = x;
+        this.y = y;
+    }
+    public void up()
+    {
+        this.y = y+1;
+        System.out.println("I am the "+this.kind+", I am now standing on square"+this.x+", "+this.y);
+    }
+
+    public void down()
+    {
+        this.y = y-1;
+        System.out.println("I am the "+this.kind+", I am now standing on square"+this.x+", "+this.y);
+    }
+
+    public void left()
+    {
+        this.x = x-1;
+        System.out.println("I am the "+this.kind+", I am now standing on square"+this.x+", "+this.y);
+    }
+
+    public void right()
+    {
+        this.x = x+1;
+        System.out.println("I am the "+this.kind+", I am now standing on square"+this.x+", "+this.y);
+    }
+
+    public int getX()
+    {
+        return x;
+    }
+
+    public int getY()
+    {
+        return y;
+    }
+
+}
---
+++ Main.java
@@ -0,0 +1,63 @@
+public class Main
+{
+    public static void main(String[] args)
+    {
+        Animals predator = new Animals("Snake", 0, 3);
+        Animals prey = new Animals("Rabbit",-1 ,1 );
+        prey.right();
+        {
+            //Checking if Snake sees the Rabbit
+            if(prey.getX()==predator.getX() && (predator.getY()-prey.getY())==1)
+        {
+            System.out.println("Nooooo Please don't eat me!!!");
+            predator.down();
+            System.out.println("Omn omn omn I'm eating you wahahahahahaa!!!");
+        }
+        if(prey.getX()==predator.getX() && (predator.getY()-prey.getY())==-1)
+        {
+            System.out.println("Nooooo Please don't eat me!!!");
+            predator.up();
+            System.out.println("Omn omn omn I'm eating you wahahahahahaa!!!");
+        }
+        if(prey.getY()==predator.getY() && (predator.getX()-prey.getX())==1)
+        {
+            System.out.println("Nooooo Please don't eat me!!!");
+            predator.right();
+            System.out.println("Omn omn omn I'm eating you wahahahahahaa!!!");
+        }
+        if(prey.getY()==predator.getY() && (predator.getX()-prey.getX())==-1)
+        {
+            System.out.println("Nooooo Please don't eat me!!!");
+            predator.left();
+            System.out.println("Omn omn omn I'm eating you wahahahahahaa!!!");
+        }}
+        predator.down();
+        {
+            //Checking if Snake sees the Rabbit
+            if(prey.getX()==predator.getX() && (predator.getY()-prey.getY())==1)
+            {
+                System.out.println("Nooooo Please don't eat me!!!");
+                predator.down();
+                System.out.println("Omn omn omn I'm eating you wahahahahahaa!!!");
+            }
+            if(prey.getX()==predator.getX() && (predator.getY()-prey.getY())==-1)
+            {
+                System.out.println("Nooooo Please don't eat me!!!");
+                predator.up();
+                System.out.println("Omn omn omn I'm eating you wahahahahahaa!!!");
+            }
+            if(prey.getY()==predator.getY() && (predator.getX()-prey.getX())==1)
+            {
+                System.out.println("Nooooo Please don't eat me!!!");
+                predator.right();
+                System.out.println("Omn omn omn I'm eating you wahahahahahaa!!!");
+            }
+            if(prey.getY()==predator.getY() && (predator.getX()-prey.getX())==-1)
+            {
+                System.out.println("Nooooo Please don't eat me!!!");
+                predator.left();
+                System.out.println("Omn omn omn I'm eating you wahahahahahaa!!!");
+            }}
+
+    }
+}
