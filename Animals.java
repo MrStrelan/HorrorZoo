b7e43f0da2d494d427558447fa02bc0ae6b82b02
Adam0Schulz <adam@schulz.sk>
2021-09-13 14:29:04 +0200

    this is a test - change: comment attributes

--- Animals.java
+++ Animals.java
@@ -1,46 +1,41 @@
-public class Animals
-{
+public class Animals {
+
+    // Attributes
     private String kind;
     private int x;
     private int y;
 
-    public Animals(String kind, int x, int y)
-    {
+    public Animals(String kind, int x, int y) {
         this.kind = kind;
         this.x = x;
         this.y = y;
     }
-    public void up()
-    {
-        this.y = y+1;
-        System.out.println("I am the "+this.kind+", I am now standing on square"+this.x+", "+this.y);
+
+    public void up() {
+        this.y = y + 1;
+        System.out.println("I am the " + this.kind + ", I am now standing on square" + this.x + ", " + this.y);
     }
 
-    public void down()
-    {
-        this.y = y-1;
-        System.out.println("I am the "+this.kind+", I am now standing on square"+this.x+", "+this.y);
+    public void down() {
+        this.y = y - 1;
+        System.out.println("I am the " + this.kind + ", I am now standing on square" + this.x + ", " + this.y);
     }
 
-    public void left()
-    {
-        this.x = x-1;
-        System.out.println("I am the "+this.kind+", I am now standing on square"+this.x+", "+this.y);
+    public void left() {
+        this.x = x - 1;
+        System.out.println("I am the " + this.kind + ", I am now standing on square" + this.x + ", " + this.y);
     }
 
-    public void right()
-    {
-        this.x = x+1;
-        System.out.println("I am the "+this.kind+", I am now standing on square"+this.x+", "+this.y);
+    public void right() {
+        this.x = x + 1;
+        System.out.println("I am the " + this.kind + ", I am now standing on square" + this.x + ", " + this.y);
     }
 
-    public int getX()
-    {
+    public int getX() {
         return x;
     }
 
-    public int getY()
-    {
+    public int getY() {
         return y;
     }
 
